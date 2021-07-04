06f906a989035a371ea95d2bdde00f976b892784
jhonnyjc <jhan18jhon@gmail.com>
2021-07-03 21:24:33 -0400

    integracion con bootstrap

---
+++ application/views/formulario.php
@@ -0,0 +1,15 @@
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      Column10
+    </div>
+    <div class="col-md-4">
+      Column20
+    </div>
+    <div class="col-md-4">
+      Column30
+    </div>
+  </div>
+</div>
+
---
+++ application/views/welcome_message.php
@@ -0,0 +1,22 @@
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      Column1
+    </div>
+    <div class="col-md-4">
+      Column2
+    </div>
+    <div class="col-md-4">
+      Column3
+    </div>
+  </div>
+</div>
+
+<script src="<?php echo base_url();?>bootstrap/js/bootstrap.js"> </script>
+<script src="<?php echo base_url();?>bootstrap/js/bootstrap.js"> </script>
+<script src="<?php echo base_url();?>bootstrap/js/bootstrap.js"> </script>
+
+</body>
+</html>
